22836bbc12fae8b9b7d0c8b45fe830ff9a47e006
efomenko <efomenkoone@yandex.ru>
2023-07-27 15:52:53 +0500

    ref: change name const in FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 @Slf4j
 @RequestMapping("/films")
 public class FilmController extends Controller {
-    private final String SERVICE = "FilmService";
+    private final static String USE_SERVICE = "FilmService";
 
     public FilmController(Service filmService) {
         super(filmService);
