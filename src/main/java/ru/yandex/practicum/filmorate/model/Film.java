61f7cb89635186e4bed91c3dc11e97eb0d91cf50
efomenko <efomenkoone@yandex.ru>
2023-08-03 22:06:10 +0500

    ref:добавил информацию в readme.md ref:добавил дополнительные поля в сущности

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class Film extends Model {
@@ -11,5 +12,7 @@ public class Film extends Model {
     private LocalDate releaseDate;
     @Positive
     private int duration;
+    private List<String> genre;
+    private MPAStatus mpa;//Motion Picture Association
 
 }
